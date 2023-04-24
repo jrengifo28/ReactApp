23410ea2ac9552f89837b5ed018425414e5e87a3
Jhonathan Rengifo <jrengifo28@hotmail.com>
2023-04-23 20:10:31 -0500

    Choosing the State Structure

--- src/App.tsx
+++ src/App.tsx
@@ -4,23 +4,34 @@ import { useState } from "react";
 function App() {
   //let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
 
-  const [isVisible, setVisibility] = useState(false);
-  const [isApproved, setApproved] = useState(true);
+  // const [isVisible, setVisibility] = useState(false);
+  // const [isApproved, setApproved] = useState(true);
+
+  // const [firstName, setFirstName] = useState("");
+  // const [lastName, setlastName] = useState("");
+  const [person, setPerson] = useState({
+    firstName: "",
+    lastName: "",
+  });
+  const [isLoading, setLoading] = useState(false);
+
+  // const fullName = firstName + ' ' + lastName
 
   /*const handleSelectItem = (item: string) => {
     console.log(item);
   }
   */
 
-  const handleClick = () => {
-    setVisibility(true);
-    console.log(isVisible);
-  };
+  // const handleClick = () => {
+  //   setVisibility(true);
+  //   console.log(isVisible);
+  // };
 
   //return <div><ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem}/></div>
   return (
     <div>
-      <button onClick={handleClick}>Show</button>
+      {/* <button onClick={handleClick}>Show</button>  */ firstName}
+      {lastName}
     </div>
   );
 }
