469ad69da6aa58b3b408f0aee43113e45203b71c
Jhonathan Rengifo <jrengifo28@hotmail.com>
2023-04-25 15:44:44 -0500

    Exercise- Updating State Ex3

--- src/App.tsx
+++ src/App.tsx
@@ -105,14 +105,32 @@ function App() {
   //   setGame({ ...game, player: { ...game.player, name: "Bob" } });
   // };
 
-  //Exercise 2
-  const [pizza, setPizza] = useState({
-    name: "Spicy Pepperoni",
-    toppings: ["Mushroom"],
+  // //Exercise 2
+  // const [pizza, setPizza] = useState({
+  //   name: "Spicy Pepperoni",
+  //   toppings: ["Mushroom"],
+  // });
+
+  // const handleClick = () => {
+  //   setPizza({ ...pizza, toppings: [...pizza.toppings, "Cheese"] });
+  // };
+
+  //Exercise 3
+  const [cart, setCart] = useState({
+    discount: 0.1,
+    items: [
+      { id: 1, title: "Product 1", quantity: 1 },
+      { id: 2, title: "Product 2", quantity: 1 },
+    ],
   });
 
   const handleClick = () => {
-    setPizza({ ...pizza, toppings: [...pizza.toppings, "Cheese"] });
+    setCart({
+      ...cart,
+      items: cart.items.map((item) =>
+        item.id === 1 ? { ...item, quantity: item.quantity + 1 } : item
+      ),
+    });
   };
 
   return (
