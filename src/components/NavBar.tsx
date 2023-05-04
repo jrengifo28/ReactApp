29889ffcbe010fc0d6ea29c383c53321210d21b1
Jhonathan Rengifo <jrengifo28@hotmail.com>
2023-05-04 16:52:11 -0500

    Building the Navigation Bar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,13 @@
-import React from "react";
+import { HStack, Image, Text } from "@chakra-ui/react";
+import logo from "../assets/logo.webp";
 
-interface Props {
-  carItemsCount: number;
-}
-
-const NavBar = ({ carItemsCount }: Props) => {
-  return <div>NavBar: {carItemsCount}</div>;
+const NavBar = () => {
+  return (
+    <HStack>
+      <Image src={logo} boxSize="60px" />
+      <Text>NavBar</Text>
+    </HStack>
+  );
 };
 
 export default NavBar;
