dfe8e6d63a9fef018fa2cfdd8a622103390ef4a7
Jhonathan Rengifo <jrengifo28@hotmail.com>
2023-06-24 08:59:00 -0500

    Ex - Removing Props

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,7 +1,7 @@
 import { useInfiniteQuery } from "@tanstack/react-query";
 import ms from 'ms';
-import { GameQuery } from "../App";
 import APIClient, { FetchResponse } from "../services/api-client";
+import useGameQueryStore from "../store";
 import { Platform } from "./usePlatforms";
 
 const apiClient = new APIClient<Game>('/games');
@@ -16,24 +16,26 @@ export interface Game {
   }
   
   
-const useGames = (gameQuery: GameQuery) => 
-
-useInfiniteQuery<FetchResponse<Game>, Error>({
-  queryKey: ['games', gameQuery],
-  queryFn: ({ pageParam = 1}) =>
-  apiClient.getALL({
-    params: {
-      genres: gameQuery.genreId,
-      parent_platforms: gameQuery.platformId,
-      ordering: gameQuery.sortOrder,
-      search: gameQuery.searchText,
-      page: pageParam
+const useGames = () => {
+  const gameQuery = useGameQueryStore(s => s.gameQuery);
+  
+  return useInfiniteQuery<FetchResponse<Game>, Error>({
+    queryKey: ['games', gameQuery],
+    queryFn: ({ pageParam = 1}) =>
+    apiClient.getALL({
+      params: {
+        genres: gameQuery.genreId,
+        parent_platforms: gameQuery.platformId,
+        ordering: gameQuery.sortOrder,
+        search: gameQuery.searchText,
+        page: pageParam
+      },
+    }),
+    getNextPageParam: (lastPage, allPages) => {
+      return lastPage.next ? allPages.length + 1: undefined;
     },
-  }),
-  getNextPageParam: (lastPage, allPages) => {
-    return lastPage.next ? allPages.length + 1: undefined;
-  },
-  staleTime: ms('24h'),
-});
+    staleTime: ms('24h'),
+  });
+}
 
 export default useGames;
